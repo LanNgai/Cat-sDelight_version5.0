b8d3f31efc6b03fc59b072d44517a302afd279d6
Fausta Pavalkyte <B00165884@mytudublin.ie>
2025-03-14 12:37:03 +0000

    Updated database tables

--- data/init.sql
+++ data/init.sql
@@ -3,33 +3,41 @@ CREATE DATABASE catsdelight;
 
 
 use catsdelight;
-    CREATE TABLE user (
-        UserID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    CREATE TABLE login (
+        LoginID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         Username VARCHAR(45) NOT NULL,
         Email VARCHAR(45) NOT NULL,
-        Password VARCHAR(300) NOT NULL,
-        Bio VARCHAR(300) DEFAULT 'Hello! I am a cat owner, I hope to find the best products for my kitties!',
-        TotalLikes INT DEFAULT 0
+        Password VARCHAR(300) NOT NULL
     );
 
     CREATE TABLE administrator (
-        UserID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        FOREIGN KEY (UserID) REFERENCES user(UserID)
+        AdminLoginID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        FOREIGN KEY (AdminLoginID) REFERENCES login(LoginID)
     );
 
-    CREATE TABLE regularUser (
-        UserID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        FOREIGN KEY (UserID) REFERENCES user(UserID)
+    CREATE TABLE user (
+        UserLoginID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        FOREIGN KEY (UserLoginID) REFERENCES login(LoginID)
+    );
+    
+    CREATE TABLE profile (
+		ProfileID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        UserLoginID INT,
+        FOREIGN KEY (UserLoginID) REFERENCES user(UserLoginID),
+        Bio VARCHAR(300) DEFAULT 'Hello! I am a cat owner, I hope to find the best products for my kitties!',
+        ProfileImage VARCHAR(45),
+        TotalLikes INT DEFAULT 0
     );
 
     CREATE TABLE products(
         ProductID INT PRIMARY KEY AUTO_INCREMENT,
-        Administrator_UserID INT UNSIGNED,
-        FOREIGN KEY (Administrator_UserID) REFERENCES administrator(UserID),
+        AdminLoginID INT UNSIGNED,
+        FOREIGN KEY (AdminLoginID) REFERENCES administrator(AdminLoginID),
         ProductName VARCHAR(45) NOT NULL,
         ProductType VARCHAR(45),
         ProductDescription TEXT NOT NULL,
         ProductManufacturer VARCHAR(45) NOT NULL,
+        ProductImage VARCHAR(45) DEFAULT NULL,
         ProductLink VARCHAR(45) NOT NULL
     );
 
@@ -37,23 +45,23 @@ use catsdelight;
         ReviewID INT AUTO_INCREMENT PRIMARY KEY,
         ProductID INT,
         FOREIGN KEY (ProductID) REFERENCES products(ProductID),
-        Administrator_UserID INT UNSIGNED,
-        FOREIGN KEY (Administrator_UserID) REFERENCES administrator(UserID),
+        AdminLoginID INT UNSIGNED,
+        FOREIGN KEY (AdminLoginID) REFERENCES administrator(AdminLoginID),
         QualityRating INT,
         PriceRating INT,
         ReviewText TEXT,
-        DateTime DATETIME
+        DateAndTime DATETIME
     );
 
     CREATE TABLE comments(
         CommentID INT AUTO_INCREMENT PRIMARY KEY,
         ReviewID INT,
         FOREIGN KEY (ReviewID) REFERENCES reviews(ReviewID),
-        RegularUser_UserID INT UNSIGNED,
-        FOREIGN KEY (RegularUser_UserID) references regularUser(UserID),
+        UserLoginID INT UNSIGNED,
+        FOREIGN KEY (UserLoginID) references user(UserLoginID),
         CommentText TEXT NOT NULL,
         Likes INT DEFAULT 0,
-        DateTime DATETIME
+        DateAndTime DATETIME
     );
 
 INSERT INTO catsdelight.user (UserID, Username, Email, Password, Bio, TotalLikes) VALUES (1, 'Lan', 'lanngai79@gmail.com', '123', 'Hello! I am a cat owner, I hope to find the best products for my kitties!', 0);
