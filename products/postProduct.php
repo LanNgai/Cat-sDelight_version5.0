eed60bcf20615d03153b1dec21f78bcf1a9c0d34
Fausta Pavalkyte <B00165884@mytudublin.ie>
2025-05-04 14:28:18 +0100

    Update postProduct.php

--- products/postProduct.php
+++ products/postProduct.php
@@ -4,24 +4,24 @@
     require "../backend/DBconnect.php";
     if ($_SERVER['REQUEST_METHOD'] === 'POST')
     {
-//        if (!isset($_SESSION['admin_id'])) {
-//            throw new Exception("You must be logged in as an administrator to add products.");
-//        }
-
+        //sanitise and retrieve form inputs
         $productName = clean($_POST['pName']);
         $productType = clean($_POST['category']);
         $manufacturer = clean($_POST['manufacturer']);
         $description = clean($_POST['description']);
         $productLink = filter_var(clean($_POST['product_link']), FILTER_SANITIZE_URL);
 
+        //check if everything has been filled out
         if (empty($productName) || empty($productType) || empty($manufacturer) || empty($description) || empty($productLink)) {
             throw new Exception("All fields are required.");
         }
 
+        //sqp to add product into database
         $sql = "INSERT INTO products (AdminLoginID, ProductName, ProductType, ProductDescription, ProductManufacturer, ProductImage, ProductLink)
                 VALUES (:adminLoginID, :productName, :productType, :description, :manufacturer, :productImage, :productLink)";
 
         try {
+            //execute sql and add to database
             $stmt = $conn->prepare($sql);
             $stmt->execute([
                 ':adminLoginID' => $_SESSION['admin_id'],
